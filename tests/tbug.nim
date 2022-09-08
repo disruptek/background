316bf3c59d6e915391a22382ccfd7da18da5338b
Smooth Operator <github@andy.disruptek.com>
2022-09-08 15:51:20 -0400

    add a broken test

---
+++ tests/tbug.nim
@@ -0,0 +1,23 @@
+import pkg/balls
+
+import background
+
+suite "how to crash it":
+  block:
+    ## failing successfully
+
+    # define the function to run remotely
+    proc greetingsFromSomewhereElse(x: int): int =
+      echo "hello from thread ", getThreadId()
+      return x + 1
+
+    # set the stage for the movement
+    echo "spawning background call from ", getThreadId()
+
+    # run the function in the background
+    let k = background greetingsFromSomewhereElse(3)
+
+    echo "recovering result in ", getThreadId()
+    assert 4 == recover k
+
+    echo "received result in ", getThreadId()
